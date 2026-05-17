a675f1075a7177f5e8e9d18a6029086a785d82dd
Kynmmarshall <kynmmarshall@gmail.com>
2026-05-17 15:29:08 +0100

    Online version working!

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -5,9 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/health_assistance?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Kamdeu2007."; // Change to your MySQL password
+    private static final String DEFAULT_URL =
+        "jdbc:mysql://38.242.246.126:3306/health_assistance?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String DEFAULT_USER = "health_user";
+    private static final String DEFAULT_PASSWORD = "StrongPassword123!";
+
+    private static final String URL =
+        System.getenv().getOrDefault("HAS_DB_URL", DEFAULT_URL);
+    private static final String USER =
+        System.getenv().getOrDefault("HAS_DB_USER", DEFAULT_USER);
+    private static final String PASSWORD =
+        System.getenv().getOrDefault("HAS_DB_PASSWORD", DEFAULT_PASSWORD);
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
